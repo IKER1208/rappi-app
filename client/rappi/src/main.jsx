d742fe8bcd0c6fde156a32999738a2e6aa01d081
EGuereca <jeguerecamunoz@gmail.com>
2025-02-05 12:32:02 -0600

    mapa mamalon

--- client/rappi/src/main.jsx
+++ client/rappi/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import GoogleMap from './components/Map.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <GoogleMap lat={37.7749} lng={-122.4194} />
   </StrictMode>,
 )
